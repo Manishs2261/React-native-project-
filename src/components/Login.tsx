b554fa3107e8eb934bb1e6e2662f6dbc1ab4b314
RiverhouseDevice <rht0x2@gmail.com>
2024-06-06 18:20:13 +0530

    initial commit

---
+++ src/components/Login.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View } from 'react-native'
+import { Text } from 'react-native-svg'
+
+function Login() {
+  return (
+    <View style={{backgroundColor:'blue'}}>
+        <Text>
+            Login
+        </Text>
+    </View>
+  )
+}
+
+export default Login
---
+++ src/globles/images.tsx
@@ -0,0 +1,4 @@
+ export const Images={
+    Applogo:require("../assets/image/Layer_1 copy.png"),
+    BackgroundImages:require('../assets/image/splash_background_image.png')
+ }
