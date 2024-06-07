17ba4abaef6a95f6eac0d068699a33ba8ca07ce2
RiverhouseDevice <rht0x2@gmail.com>
2024-06-07 14:40:25 +0530

    add scroll bar

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,6 +3,36 @@ import { Image, ScrollView, Text, View } from "react-native";
  
 
 const Home =()=>{
+
+    const items = Array.from({ length: 10 }, (_, index) => (
+        <View key={index} style={{ height: 215, width: 148, backgroundColor: '#ddebe0', borderRadius: 5, padding: 10, marginRight: 10 }}>
+          <Image source={require('../assets/image/mobile_1.png')} style={{ alignSelf: 'center', position: 'absolute', marginTop: 10 }} />
+          <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', width: 130, height: 32, borderWidth: 1, borderColor: 'yellow', alignSelf: 'center', borderRadius: 1.5, marginTop: 110, backgroundColor: 'white' }}>
+            <View>
+              <Text style={{ fontSize: 15, fontWeight: '500', color: "black" }}>30</Text>
+              <Text style={{ fontSize: 5, fontWeight: '500' }}>Days</Text>
+            </View>
+            <Text>:</Text>
+            <View>
+              <Text style={{ fontSize: 15, fontWeight: '500', color: "black" }}>20</Text>
+              <Text style={{ fontSize: 5, fontWeight: '500' }}>Hours</Text>
+            </View>
+            <Text style={{ color: 'black' }}>:</Text>
+            <View>
+              <Text style={{ fontSize: 15, fontWeight: '500', color: "black" }}>15</Text>
+              <Text style={{ fontSize: 5, fontWeight: '500' }}>Minutes</Text>
+            </View>
+            <Text style={{ color: 'black' }}>:</Text>
+            <View>
+              <Text style={{ fontSize: 15, fontWeight: '500', color: "black" }}>10</Text>
+              <Text style={{ fontSize: 5, fontWeight: '500' }}>Seconds</Text>
+            </View>
+          </View>
+          <Text style={{ fontSize: 12, fontWeight: '300', color: 'black', marginTop: 5 }}>Apple</Text>
+          <Text style={{ fontSize: 12, fontWeight: '300', color: 'black' }}>iPhone 13 Pro</Text>
+          <Text style={{ fontSize: 17, fontWeight: '700', color: 'black' }}>SAR 2600</Text>
+        </View>
+      ));
     return(
         <View>
            
@@ -13,19 +43,12 @@ const Home =()=>{
 
                  <ScrollView>
                     <View style={{padding:20}}>
-                    <View style={{height:200,width:380,backgroundColor:'black',borderRadius:7, alignSelf:'center'}}></View>
-                <Text style={{fontSize:15,fontWeight:'500',color:'black',paddingTop:20}}>Specials</Text>
+                    <View style={{height:200,width:380,backgroundColor:'black',borderRadius:7, alignSelf:'center',}}></View>
+                <Text style={{fontSize:15,fontWeight:'500',color:'black',paddingTop:20,paddingBottom:20}}>Specials</Text>
 
-                <View style={{height:215,width:148 ,backgroundColor:'#ddebe0'}}>
-                    <Image source={require('../assets/image/mobile_1.png')}  style={{alignSelf:'center' ,position:'absolute',}}></Image>
-                    <View style={{width:130,height:30, borderWidth:1,borderColor:'yellow',alignSelf:'center',borderRadius:1.5 ,marginTop:120,backgroundColor:'white'}}>
-
-                        <Text style={{fontSize:15,fontWeight:'500',color:"black"}}>30</Text>
-                    </View>
-                    <Text style={{fontSize:12,fontWeight:'300',color:'black'}}>Apple</Text>
-                    <Text style={{fontSize:12,fontWeight:'300',color:'black'}}>iPhone 13 Pro</Text>
-                    <Text style={{fontSize:17,fontWeight:'700',color:'black'}}>SAR 2600</Text>
-                </View>
+                <ScrollView horizontal={true} style={{ padding: 10 }}>
+      {items}
+      </ScrollView>
                     </View>
                  </ScrollView>
                 
