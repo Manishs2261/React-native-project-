9b990fc4e9f5da1502401fbb2914c0a9bc3187ad
RiverhouseDevice <rht0x2@gmail.com>
2024-06-13 18:50:39 +0530

    create checkout and profile screen

---
+++ src/components/CheckOutScreen.tsx
@@ -0,0 +1,184 @@
+import {View, Text, ScrollView, TextInput} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+const CheckOutScreen = () => {
+  return (
+    <ScrollView>
+     <View>
+     <View>
+
+        <View
+          style={{
+            backgroundColor: 'white',
+            width: '100%',
+            height: 60,
+            elevation: 1,
+            alignItems: 'center',
+            flexDirection: 'row',
+            paddingHorizontal: 20,
+          }}>
+          <Ionicons
+            name="arrow-back"
+            size={25}
+            style={{color: 'black'}}></Ionicons>
+          <Text
+            style={{
+              color: 'black',
+              fontSize: 25,
+              fontWeight: '500',
+              marginLeft: 100,
+            }}>
+            CHECKOUT
+          </Text>
+        </View>
+         
+      </View>
+
+
+     <View style={{margin:20}}>
+     <Text style={{color: 'black', fontSize: 15, fontWeight: '600', marginBottom:20}}>
+          Personal Details
+        </Text>
+
+      <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+        <View>
+                <View style={{flexDirection: 'row',marginBottom:10}}>
+                    <Text style={{color: 'red'}}>*</Text>
+                    <Text style={{color: 'black'}}>First Name</Text>
+                </View>
+          <View
+            style={{
+              height: 40,
+              width: 170,
+              borderColor: 'grey',
+              borderWidth: 1,
+              borderRadius: 7,
+            }}>
+            <TextInput></TextInput>
+          </View>
+        </View>
+
+        <View>
+          <View style={{flexDirection: 'row',marginBottom:10}}>
+            <Text style={{color: 'red'}}>*</Text>
+            <Text style={{color: 'black'}}>Last Name</Text>
+          </View>
+          <View
+            style={{
+              height: 40,
+              width: 170,
+              borderColor: 'grey',
+              borderWidth: 1,
+              borderRadius: 7,
+            }}>
+            <TextInput></TextInput>
+          </View>
+        </View>
+
+
+    
+
+
+ 
+      </View>
+
+
+         <View style={{flexDirection:'row',marginTop:20,}}>
+            <Text style={{color:'red'}}>*</Text>
+            <Text style={{color:"black",fontSize:15,fontWeight:'600',marginBottom:10}}>Email Address</Text>
+         </View>
+
+        <View style={{height:40,width:"100%",borderColor:'black',borderRadius:7,borderWidth:1 }}></View>
+
+        <View style={{flexDirection:'row',marginTop:20,marginBottom:10}}>
+            <Text style={{color:'red'}}>*</Text>
+            <Text style={{color:"black",fontSize:15,fontWeight:'600'}}>Phone Number</Text>
+         </View>
+
+        <View style={{height:40,width:"100%",borderColor:'black',borderRadius:7,borderWidth:1}}>
+            <View style={{height:38,width:30,backgroundColor:'yellow',borderTopLeftRadius:7,borderBottomLeftRadius:7,justifyContent:'center',alignItems:'center'}}>
+                <Text style={{color:'black',}}>+91</Text>
+            </View>
+        </View>
+
+
+         
+           
+            <Text style={{color:"black",fontSize:15,fontWeight:'600'}}>Company Name</Text>
+
+        <View style={{height:40,width:"100%",borderColor:'black',borderRadius:7,borderWidth:1}}></View>
+
+
+        <View style={{flexDirection:'row'}}>
+            <Text style={{color:'red'}}>*</Text>
+            <Text style={{color:"black",fontSize:15,fontWeight:'600'}}>Address line 1</Text>
+         </View>
+
+        <View style={{height:40,width:"100%",borderColor:'black',borderRadius:7,borderWidth:1}}></View>
+
+
+       
+             
+            <Text style={{color:"black",fontSize:15,fontWeight:'600'}}>Address line 2</Text>
+        <View style={{height:40,width:"100%",borderColor:'black',borderRadius:7,borderWidth:1}}></View>
+
+        <View style={{flexDirection:'row'}}>
+            <Text style={{color:'red'}}>*</Text>
+            <Text style={{color:"black",fontSize:15,fontWeight:'600'}}>City</Text>
+         </View>
+
+        <View style={{height:40,width:"100%",borderColor:'black',borderRadius:7,borderWidth:1}}></View>
+
+        <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+        <View>
+                <View style={{flexDirection: 'row'}}>
+                    <Text style={{color: 'red'}}>*</Text>
+                    <Text style={{color: 'black'}}>Country</Text>
+                </View>
+          <View
+            style={{
+              height: 40,
+              width: 180,
+              borderColor: 'grey',
+              borderWidth: 1,
+              borderRadius: 7,
+            }}>
+             <MaterialIcons name='keyboard-arrow-down' size={30} color={"black"}></MaterialIcons>
+          </View>
+        </View>
+
+        <View>
+          <View style={{flexDirection: 'row'}}>
+            <Text style={{color: 'red'}}>*</Text>
+            <Text style={{color: 'black'}}>Region</Text>
+          </View>
+          <View
+            style={{
+              height: 40,
+              width: 180,
+              borderColor: 'grey',
+              borderWidth: 1,
+              borderRadius: 7,
+            }}>
+             <MaterialIcons name='keyboard-arrow-down' size={30} color={"black"}></MaterialIcons>
+          </View>
+        </View>
+ 
+      </View>
+
+      <View style={{height:61,width:362,backgroundColor:'yellow',borderRadius:7,alignSelf:'center',justifyContent:'center',alignItems:'center'}}>
+        <Text style={{color:'black',fontSize:25,fontWeight:'600'}}>Continue</Text>
+
+      </View>
+     </View>
+
+     </View>
+
+
+      
+    </ScrollView>
+  );
+};
+
+export default CheckOutScreen;
--- src/components/SingleProduct.tsx
+++ src/components/SingleProduct.tsx
@@ -1,89 +1,109 @@
-import { Text, View ,Image} from "react-native";
+import { Text, View ,Image, ScrollView} from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 const SingleProduct =()=>{
 
     return(
-<View>
-    <View style={{backgroundColor:'white',width:'100%',height:60,elevation:1 ,alignItems:'center', justifyContent:'space-between'  ,flexDirection:'row'}}>
+ <ScrollView>
+    <View>
+    <View style={{backgroundColor:'white',width:'100%',height:60,elevation:1 ,alignItems:'center', justifyContent:'space-between'  ,flexDirection:'row', paddingHorizontal:20}}>
 
       <Ionicons name="arrow-back" size={25} style={{color:'black'}}></Ionicons>
       <Text style={{color:'black',fontSize:25,fontWeight:'500'}} >Apple iPhone 12</Text>
-      <Ionicons name="arrow-back" size={25}></Ionicons>
+      <Ionicons name="search-outline" color='black' size={25}></Ionicons>
 
     </View>
 
+    <View style={{paddingHorizontal:20}}>
     <View style={{height:400,width:400,backgroundColor:'#e6eaf0', justifyContent:"center",alignItems:'center',borderRadius:10 , alignSelf:'center'}}>
         <Image source={require('../assets/image/mobile_1.png')} style={{height:345,width:250, }} ></Image>
 
     </View>
 
-    <View style={{flexDirection:'row',justifyContent:'space-between'}}>
+    <View style={{flexDirection:'row',justifyContent:'space-between' , marginTop:30 ,alignItems:'center'}}>
          <View style={{alignItems:'center'}}>
-         <Text>SAR 2600</Text>
-         <Text>SAR 2600</Text>
+         <Text style={{fontSize:30,fontWeight:'600',color:'black'}}>SAR 2600</Text>
+         <Text style={{fontSize:15,fontWeight:'600', textDecorationLine:"line-through" ,alignSelf:'flex-start'}}>SAR 2600</Text>
          </View>
-        <View style={{height:30,width:202,borderRadius:7,borderColor:"gray",borderWidth:1,flexDirection:'row',justifyContent:'space-evenly'}}>
+        <View style={{height:30,width:202,borderRadius:7,borderColor:"gray",borderWidth:1,flexDirection:'row',justifyContent:'space-evenly' ,alignItems:'center'}}>
             <Text>G - Pay </Text>
             <Text>Phone pe</Text>
             <Text>UPI</Text>
         </View>
     </View>
 
-    <View style={{flexDirection:'row'}}>
+    <View style={{flexDirection:'row' ,marginTop:30}}>
         <View>
-        <Text>Brand:</Text>
-        <Text>Product Code:</Text>
-        <Text>Availability:</Text>
+        <Text style={{color:'black',fontSize:15 ,}}>Brand :</Text>
+        <Text style={{color:'black',fontSize:15 ,}}>Product Code :</Text>
+        <Text style={{color:'black',fontSize:15 ,}}>Availability :</Text>
         </View>
+        <View style={{height:60,width:1,backgroundColor:'grey' ,marginHorizontal:30}}></View>
         <View>
-       <Text>Apple</Text>
-       <Text>#123</Text>
-       <Text>in Stock</Text>
+       <Text style={{color:'black',fontSize:15 ,}} >Apple</Text>
+       <Text style={{color:'black',fontSize:15 ,}}>#123</Text>
+       <Text style={{color:'black',fontSize:15 ,}}>in Stock</Text>
         </View>
     </View>
 
-    <Text>Conditions:</Text>
-    <View style={{flexDirection:'row',justifyContent:"space-evenly"}}>
-        <View style={{height:40,width:40,borderColor:'grey',borderWidth:1,}}>
-            <Text>Good</Text>
+    <Text style={{color:"black",fontWeight:"600",marginTop:30}}>Conditions :</Text>
+    <View style={{flexDirection:'row',justifyContent:"flex-start" ,marginTop:20}}>
+        <View style={{height:40,width:80,borderColor:'grey',borderWidth:1,borderRadius:7,justifyContent:"center",alignItems:"center"}}>
+            <Text style={{color:'black'}}>Good</Text>
         </View>
-        <View style={{height:40,width:40,borderColor:'grey',borderWidth:1,}}>
-            <Text>Very Good</Text>
+        <View style={{height:40,width:80,borderColor:'grey',borderWidth:1,borderRadius:7,justifyContent:"center",alignItems:"center" ,marginHorizontal:20}}>
+            <Text style={{color:'black'}}>Very Good</Text>
         </View>
-        <View style={{height:40,width:40,borderColor:'grey',borderWidth:1,}}>
-            <Text>Best</Text>
+        <View style={{height:40,width:80,borderColor:'grey',borderWidth:1,borderRadius:7,justifyContent:"center",alignItems:"center"}}>
+            <Text style={{color:'black'}}>Best</Text>
         </View>
     </View>
 
-    <Text>Size</Text>
+    <Text style={{fontSize:15,fontWeight:"600",color:'black', marginTop:30,marginBottom:20}}>Size :</Text>
     <View style={{flexDirection:'row'}}>
-    <View style={{height:40,width:40,borderColor:'grey',borderWidth:1,}}>
-            <Text>Good</Text>
+    <View style={{height:40,width:80,borderColor:'grey',borderWidth:1,borderRadius:7,justifyContent:"center",alignItems:"center",marginRight:20}}>
+            <Text style={{color:'black'}}>128GB</Text>
         </View>
-        <View style={{height:40,width:40,borderColor:'grey',borderWidth:1,}}>
-            <Text>Good</Text>
+        <View style={{height:40,width:80,borderColor:'grey',borderWidth:1,borderRadius:7,justifyContent:"center",alignItems:"center"}}>
+            <Text style={{color:"black"}}>512GB</Text>
         </View>
     </View>
 
-    <Text>Color</Text>
+    <Text style={{color:'black',marginTop:30,fontSize:15,fontWeight:'600',marginBottom:10}}>Color :</Text>
     <View>
         <View style={{height:40,width:40,backgroundColor:'black',borderRadius:20}}></View>
     </View>
 
-<Text>Warranty:</Text>
+<Text style={{color:'black',fontSize:15,fontWeight:'600',marginTop:30,marginBottom:10}}>Warranty:</Text>
 
-<View style={{flexDirection:'row'}}>
-<View style={{height:40,width:40,borderColor:'grey',borderWidth:1,}}>
-            <Text>Good</Text>
+<View style={{flexDirection:'row', marginBottom:30}}>
+<View style={{height:40,width:90,borderColor:'grey',borderWidth:1,borderRadius:7,justifyContent:"center",alignItems:"center",marginRight:20}}>
+            <Text style={{color:'black'}}>12 Months</Text>
         </View>
-        <View style={{height:40,width:40,borderColor:'grey',borderWidth:1,}}>
-            <Text>Good</Text>
+        <View style={{height:40,width:90,borderColor:'grey',borderWidth:1,borderRadius:7,justifyContent:"center",alignItems:"center"}}>
+            <Text style={{color:'black'}}>24 Months</Text>
         </View>
 </View>
+    </View>
+
+    <View style={{width:"100%", height:1, backgroundColor:'grey', marginBottom:8 ,marginHorizontal:20}}></View>
+     <View style={{flexDirection:'row',justifyContent:'space-evenly'}}>
+        <View style={{flexDirection:'row'}}>
+        <Ionicons name="star-outline" color="#ad930e" size={25} style={{marginHorizontal:3}}></Ionicons>
+        <Ionicons name="star-outline" color="#ad930e" size={25} style={{marginHorizontal:3}}></Ionicons>
+        <Ionicons name="star-outline" color="#ad930e" size={25} style={{marginHorizontal:3}}></Ionicons>
+        <Ionicons name="star-outline" color="#ad930e" size={25} style={{marginHorizontal:3}}></Ionicons>
+        <Ionicons name="star-outline" color="#ad930e" size={25} style={{marginHorizontal:3}}></Ionicons>
+        
+        </View>
+        <Text style={{color:'black'}}>0 Reviews</Text>
+        <Text style={{color:'black'}}> Write a Reviews</Text>
+     </View>
+    <View style={{width:"100%", height:1, backgroundColor:'grey', marginBottom:20 ,marginHorizontal:20 ,marginTop:8}}></View>
     
 
 </View>
+ </ScrollView>
     );
 }
 
